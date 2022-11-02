6d54942571c233ce2ba5be962649d76784e4bb12
VeniiiD <venidachev@gmail.com>
2022-11-02 20:06:35 +0200

    Polishing up code

--- Login.php
+++ Login.php
@@ -5,7 +5,7 @@
    <label for="password">Password:</label><br>
   <input type="password" id="password" name="password" required><br>
   <form>
-  <input type="submit" value="Submit">
+  <p><input type="submit" value="Submit">
 </form>
 
 <script type="text/javascript">
