4a0577b7873dd8cbd3a154b8982094eb99fadd61
VeniiiD <101395014+VeniiiD@users.noreply.github.com>
2022-11-02 10:13:16 +0200

    Update and rename databaseConnection.php to InterfaceVariables.php

---
+++ InterfaceVariables.php
@@ -0,0 +1,26 @@
+<?php
+	//include "tempVariables.php";
+	include("Connect.php");
+	
+	$url= $_SERVER['REQUEST_URI'];    
+	$pos = strpos($url, "username=") + 9;
+	
+
+    	$username = substr($url, $pos);
+
+	$sql = "SELECT IBAN FROM Credentials WHERE Username='$username'";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+	$iban = $row["IBAN"];
+
+	if (strpos($iban, "BOKB") !== false) { $bankName = "Bank of Kolyo"; $bankNameSql = "BankOfKolyo"; }
+    	if (strpos($iban, "BOVB") !== false) { $bankName = "Bank of Veni"; $bankNameSql = "BankOfVeni"; }
+
+   	$sql = "SELECT * FROM $bankNameSql WHERE IBAN='$iban'";
+    	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+
+    	$firstname = $row["firstname"];
+    	$lastname = $row["lastname"];
+	$bal = $row["Balance"];
+    	$currency = "lv.";
