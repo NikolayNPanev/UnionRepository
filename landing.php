726b808afc2eb66785b95e3b14fe38a2c15dd1d0
NikolayNPanev <nikolay.n.panev@gmail.com>
2022-11-02 18:08:57 +0200

    index to redirect landing page

--- landing.php
+++ landing.php
@@ -1,8 +1,14 @@
 <?php
  //A test page
- include("DBFunc.php");
- include("Generator.php");
- Test();
+ //include("DBFunc.php");
+ //include("Generator.php");
+ //Test();
 
- echo GenerateIban("BankOfVeni");
+ //echo GenerateIban("BankOfVeni");
  ?>
+
+<h1>Welcome to the Union Repository</h1>
+ <form>
+ 	<a href='Login.php'><input type='button' action='Login.php' value='login'></input></a><br><br>
+ 	<a href='Register.php'><input type='button' action='Register.php' value='register' href='Register.php'></input></a><br>
+ </form>
