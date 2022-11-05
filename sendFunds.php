1bd4fe02387e3c988704f538119b107ab7d1771f
XeR0x4 <veni404@gmail.com>
2022-11-05 15:14:18 +0200

    Updating to IBAN System / Work on sending funds

--- sendFunds.php
+++ sendFunds.php
@@ -1,16 +1,3 @@
-<?php
-	include "interfaceVariables.php";
-
-	echo "<p> You currently have $bal $currency </p>";
-?>
-Please enter the information of the recepient and the amount:
-<form method="get">
-	<table>
-		<tr><td>First Name:</td><td><input type="text" name="recepientFirstName" style="width: 190px"></td>
-		<tr><td>Last Name:</td><td><input type="text" name="recepientLastName" style="width: 190px"></td>
-		<tr><td>IBAN:</td><td><input type="text" name="recepientIBAN" style="width: 190px"></td>
-		<tr><td>Amount:</td><td><input type="text" name="recepientAmount" style="width: 190px"></td>
-	</table>
-</form>
-<?php
-echo "<a href='interface.php?username=$Username'><button>Return to Main Page</button></a>"; ?>
+<?php 
+$Username = $_POST['username'];
+echo "this is where we process your sent funds, bitch ($Username)";
