0180e47c9440eeca715b1179f7d0260d73605d23
NikolayNPanev <nikolay.n.panev@gmail.com>
2022-11-16 18:13:59 +0200

    Fixed CSS Bugs

--- interface.php
+++ interface.php
@@ -10,8 +10,8 @@
 	<p> You currently have $bal $currency </p>";
   
   ?>
-        <a href='<?php echo "transactionHistoryInterface.php?iban=$iban";?>'><button style='width: 300px'>Transaction History</button></a>
-        <p><a href='<?php echo "sendFundsInterface.php?iban=$iban";?>'><button style='width: 300px'>Send Funds</button></a>
+        <a href='<?php echo "transactionHistoryInterface.php?iban=$iban";?>'><button >Transaction History</button></a>
+        <p><a href='<?php echo "sendFundsInterface.php?iban=$iban";?>'><button >Send Funds</button></a>
 
         <form method='get' action='personalFunds.php'>
         <input type='submit' value='Add funds' > <input type='text' name='addedFunds' >
--- sendFundsInterface.php
+++ sendFundsInterface.php
@@ -7,12 +7,12 @@ Please enter the information of the recepient and the amount:
 <form method="get" action="sendFunds.php">
 	
 	<table>
-		<tr><td>First Name:</td><td><input type="text" name="recepientFirstName" style="width: 190px" required></td>
-		<tr><td>Last Name:</td><td><input type="text" name="recepientLastName" style="width: 190px" required></td>
-		<tr><td>IBAN:</td><td><input type="text" name="recepientIBAN" style="width: 190px" required></td>
-		<tr><td>Amount:</td><td><input type="text" name="recepientAmount" style="width: 190px" required></td>
-		<tr><td>Reason:</td><td><input type="text" name="reason" style="width: 190x" required></td>
-		<tr><td colspan=2><input type="submit" name="submit" value="Send Funds" style="width: 270px"></td>
+		<tr><td>First Name:</td><td><input type="text" name="recepientFirstName"  required></td>
+		<tr><td>Last Name:</td><td><input type="text" name="recepientLastName" class='sendFunds' required></td>
+		<tr><td>IBAN:</td><td><input type="text" class="sendFunds" name="recepientIBAN"  required></td>
+		<tr><td>Amount:</td><td><input type="text" class="sendFunds" name="recepientAmount" required></td>
+		<tr><td>Reason:</td><td><input type="text" class="sendFunds" name="reason" required></td>
+		<tr><td colspan=2><input type="submit" class="sendFunds" name="submit" value="Send Funds" style="width:100%;"></td>
 	</table>
 	<input type="hidden" name="iban" value="<?php echo $iban;?>">
 </form>
