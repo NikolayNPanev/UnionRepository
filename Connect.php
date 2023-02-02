2a25adaada5fd12c9f86da739c7704692b204a0a
NikolayNPanev <nikolay.n.panev@gmail.com>
2023-02-02 19:20:56 +0200

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "union_rep";
+$servername = "sql205.byethost7.com";
+$username = "b7_33237327";
+$password = "959501ko";
+$database = "b7_33237327_union_repository";
 $conn = new mysqli($servername, $username, $password,$database);
 ?>
